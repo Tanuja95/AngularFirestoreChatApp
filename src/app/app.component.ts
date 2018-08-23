78c63cc79d864999876c3d261d6a613a1d68995e
crackm <tanuja.mandala@caerusskillmatic.com>
2018-08-23 16:08:18 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,15 @@
 import { Component } from '@angular/core';
-
 import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
 //import { Observable } from 'rxjs/Observable';
+//import 'rxjs/add/operator/map';
+import { map } from 'rxjs/operators';
 import { Observable } from '../../node_modules/rxjs';
-//import 'rxjs/add/operator/map'; 
-import { map } from 'rxjs/operators'
 
 interface Post {
   title: string;
   content: string;
 }
+
 
 interface PostId extends Post { 
   id: string; 
@@ -20,24 +20,32 @@ interface PostId extends Post {
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-
 export class AppComponent {
 
   postsCol: AngularFirestoreCollection<Post>;
-  posts: Observable<Post[]>;
+  posts: any;
 
   title:string;
   content:string;
 
-  constructor(private afs: AngularFirestore) {}
+  postDoc: AngularFirestoreDocument<Post>;
+  post: Observable<Post>;
+
+  constructor(private afs: AngularFirestore) {
+  }
 
   ngOnInit() {
     this.postsCol = this.afs.collection('posts');
-    this.posts = this.postsCol.valueChanges();
+    this.posts = this.postsCol.valueChanges()
   }
 
   addPost() {
-    //this.afs.collection('posts').add({'title': this.title, 'content': this.content}); 
-    this.afs.collection('posts').doc('my-custom-id').set({'title': this.title, 'content': this.content});
+    // this.afs.collection('posts').doc('my-custom-id').set({'title': this.title, 'content': this.content});
+    this.afs.collection('posts').add({'title': this.title, 'content': this.content});
+  }
+
+  getPost(postId) {
+    this.postDoc = this.afs.doc('posts/'+postId);
+    this.post = this.postDoc.valueChanges();
   }
 }
