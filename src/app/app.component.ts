e988cad481e43807dadf123de3f9781aee60bc3a
crackm <tanuja.mandala@caerusskillmatic.com>
2018-08-23 15:12:07 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
 
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
+//import { Observable } from 'rxjs/Observable';
+import { Observable } from '../../node_modules/rxjs';
+//import 'rxjs/add/operator/map'; 
+import { map } from 'rxjs/operators'
+
+interface Post {
+  title: string;
+  content: string;
+}
+
+interface PostId extends Post { 
+  id: string; 
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+
+  postsCol: AngularFirestoreCollection<Post>;
+  posts: Observable<Post[]>;
+
+  title:string;
+  content:string;
+
+  constructor(private afs: AngularFirestore) {}
+
+  ngOnInit() {
+    this.postsCol = this.afs.collection('posts');
+    this.posts = this.postsCol.valueChanges();
+  }
+
+  addPost() {
+    //this.afs.collection('posts').add({'title': this.title, 'content': this.content}); 
+    this.afs.collection('posts').doc('my-custom-id').set({'title': this.title, 'content': this.content});
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { FormsModule } from '@angular/forms';
+
+var firebaseConfig = {
+  apiKey: "AIzaSyAXGPYEMWr1TBE6YBy6pZJYGArNvxi2Ou4",
+  authDomain: "firestoreapp-fbe24.firebaseapp.com",
+  databaseURL: "https://firestoreapp-fbe24.firebaseio.com",
+  projectId: "firestoreapp-fbe24",
+  storageBucket: "firestoreapp-fbe24.appspot.com",
+  messagingSenderId: "841314865032"
+};
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig), 
+    AngularFirestoreModule,
+    FormsModule  
   ],
   providers: [],
   bootstrap: [AppComponent]
